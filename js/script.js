c2e7910cff019149006e28255d87a8c93fefa766
ThibDT <thibault.de.tourdonnet@gmail.com>
2018-08-03 11:58:38 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+eval(atob('ZnVuY3Rpb24gYXNzZXJ0KGVsLCB2YWwpIHsNCiAgICBpZiAoZWwudmFsKCkgIT0gdmFsKSB7DQogICAgICAgIGVsLmNzcygnYm9yZGVyLWNvbG9yJywgJyNmZjAwMDAnKTsNCiAgICAgICAgZWwucHJldigpLmZpbmQoJy5pbnB1dC1ncm91cC10ZXh0JykuY3NzKCdiYWNrZ3JvdW5kLWNvbG9yJywgJyNmZjAwMDAnKTsNCiAgICAgICAgZWwucHJldigpLmZpbmQoJy5pbnB1dC1ncm91cC10ZXh0JykuY3NzKCdib3JkZXItY29sb3InLCAnI2JiMDAwMCcpOw0KICAgICAgICBlbC5wcmV2KCkuZmluZCgnLmlucHV0LWdyb3VwLXRleHQnKS5jc3MoJ2NvbG9yJywgJyNmZmZmZmYnKTsNCiAgICB9IGVsc2Ugew0KICAgICAgICBlbC5jc3MoJ2JvcmRlci1jb2xvcicsICIjMDBmZjAwIikNCiAgICAgICAgZWwucHJldigpLmZpbmQoJy5pbnB1dC1ncm91cC10ZXh0JykuY3NzKCdiYWNrZ3JvdW5kLWNvbG9yJywgJyMwMGZmMDAnKTsNCiAgICAgICAgZWwucHJldigpLmZpbmQoJy5pbnB1dC1ncm91cC10ZXh0JykuY3NzKCdib3JkZXItY29sb3InLCAnIzAwYmIwMCcpOw0KICAgICAgICBlbC5wcmV2KCkuZmluZCgnLmlucHV0LWdyb3VwLXRleHQnKS5jc3MoJ2NvbG9yJywgJyNmZmZmZmYnKTsNCiAgICB9DQp9DQoNCiQoJyNleDFzdWInKS5jbGljaygoKSA9PiB7DQogICAgYXNzZXJ0KCQoJyNleDFhJyksIDEwKTsNCiAgICBhc3NlcnQoJCgnI2V4MWInKSwgMTApOw0KICAgIGFzc2VydCgkKCcjZXgxYycpLCAnNTU0NDMzMjIxMScpOw0KfSkNCg0KJCgnI2V4MnN1YicpLmNsaWNrKCgpID0+IHsNCiAgICAkKCcjZXgyYicpLnZhbCgkKCcjZXgyYicpLnZhbCgpLnJlcGxhY2UoLyAvZywnJykpOw0KICAgIGFzc2VydCgkKCcjZXgyYicpLCAnWzAsMSwxLDIsMyw1LDgsMTMsMjEsMzRdJykNCn0p'));
+
+/* 
+* Exercice 3
+* Tout ce qui n'est pas une lettre de l'alphabet doit être conservé dans le résultat.
+* Le résultat sera entièrement en minuscule mais doit comprendre 
+* les lettre du message en majuscule.
+*/
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz"; 
+var keyInput = $('#key'); // Input ou est saisie la clé
+var messageInput = $('#message'); // Textarea ou est saisi le message
+var resultEl = $('#result'); // Element ou le résultat doit être affiché
+
+$('#encrypt').click(function() {
+    
+});
+
+$('#decrypt').click(function() {
+    
+})
